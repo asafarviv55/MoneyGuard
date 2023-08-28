27bc551219cb790f374a4a456deb438d3878cf4b
asaf arviv <asaf.arviv@gmail.com>
2023-08-28 16:46:42 +0200

    Initial commit

---
+++ src/main/java/com/fintech/moneyguard/MoneyGuardApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.moneyguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fintech/moneyguard/MoneyGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.moneyguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
